282e5c40c9d40a9f388e85e1524de9299869bd12
tong92 <tong92power@gmail.com>
2023-07-14 16:54:25 +0900

    Adds facet and storage comment

--- contracts/facets/ControlFacet.sol
+++ contracts/facets/ControlFacet.sol
@@ -5,26 +5,42 @@ import "../libraries/LibDiamond.sol";
 import "../libraries/LibAppStore.sol";
 
 contract ControlFacet {
+    /** @dev gateway address 지정
+      * @param _newGateway gateway address
+      */
     function setGateway(address _newGateway) external {
         LibDiamond.enforceIsContractOwner();
         LibAppStore.setGateway(_newGateway);
     }
 
+    /** @dev approver 추가
+      * @param _newApprovers approver addresses
+      */
     function addApprovers(address[] calldata _newApprovers) external {
         LibDiamond.enforceIsContractOwner();
         LibAppStore.addApprovers(_newApprovers);
     }
 
+    /** @dev approver 비활성화
+      * @param _delApprovers approver addresses
+      */
     function removeApprovers(address[] calldata _delApprovers) external {
         LibDiamond.enforceIsContractOwner();
         LibAppStore.removeApprovers(_delApprovers);
     }
 
+    /** @dev 인증 조건 설정
+      * @param _low low boundary
+      * @param _high high boundary
+      */
     function setBoundary(uint256 _low, uint256 _high) external {
         LibDiamond.enforceIsContractOwner();
         LibAppStore.setBoundary(_low, _high);
     }
 
+    /** @dev deployed token contract address 설정
+      * @param _deployed deployed token contract address
+      */
     function setDeployedContract(address _deployed) external {
         LibDiamond.enforceIsContractOwner();
         LibAppStore.setDeployedContract(_deployed);
